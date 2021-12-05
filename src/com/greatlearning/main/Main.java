be123fadb1c415c18f9e1310b2f66972b17c557e
= <=>
2021-12-05 20:59:20 +0530

    first commit

---
+++ src/com/greatlearning/main/Main.java
@@ -0,0 +1,33 @@
+package com.greatlearning.main;
+
+import com.greatlearning.model.*;
+
+public class Main {
+	public static void main(String []args) {
+		
+		AdminDepartment admin = new AdminDepartment();
+		HrDepartment hr = new HrDepartment();
+		TechDepartment tech = new TechDepartment();
+		
+		System.out.println("Welcome to " + admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		System.out.print('\n');
+		
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.print('\n');
+		
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+	
+	}
+
+}
---
+++ src/com/greatlearning/model/TechDepartment.java
@@ -0,0 +1,22 @@
+package com.greatlearning.model;
+
+public class TechDepartment extends SuperDepartment {
+	@Override 
+	public String departmentName() {
+		return "Tech Department";
+	}
+	
+	@Override 
+	public String getTodaysWork() {
+		return "Complete coding of module 1";
+	}
+	
+	@Override 
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public String getTechStackInformation() {
+		return "core Java";
+	}
+}
